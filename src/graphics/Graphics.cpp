b791be524831e0dc47d596bab805af2caddcd3d4
satan7991 <mkuznetsov7991@gmail.com>
2017-10-29 12:15:23 +0300

    core arb from compiler

--- src/graphics/Graphics.cpp
+++ src/graphics/Graphics.cpp
@@ -20,7 +20,7 @@ application::graphics::Graphics::Graphics() {
 
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
 
     SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
     SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
